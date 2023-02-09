cff70d3e0ddeb4ecbae22a61ecdfe5a19da05862
Pol Marcet Sardà <polmarcetsarda@gmail.com>
2023-02-09 18:09:52 +0100

    Starting job to get the wrapper working

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -12046,6 +12046,7 @@ public:
   bool ActOnOmpSsDeclareTaskDirectiveWithFpga(Decl *ADecl);
   SmallVector<Decl *, 2> ompssFpgaDecls;
   bool ActOnOmpSsFpgaExtractFiles();
+  bool ActOnOmpSsFpgaGenerateWrapperCodeFiles();
 
   OSSClause *ActOnOmpSsVarListClause(
       OmpSsClauseKind Kind, ArrayRef<Expr *> Vars, SourceLocation StartLoc,
